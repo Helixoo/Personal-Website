dac0bd6d62cd794e1ac03bcbbb7424de0594ee00
Helixoo <104375707+Helixoo@users.noreply.github.com>
2022-07-29 17:23:01 +0300

    Update repos.jsx

--- pages/api/repos.jsx
+++ pages/api/repos.jsx
@@ -1,10 +1,10 @@
 const fetch = require('isomorphic-unfetch')
 
-let key1 = 'ghp_VzcC7gx0GXzOVP1';
-let key2 = '7kIiTmNtBqj2sOC0S0zxD';
+let key1 = '';
+let key2 = '';
 
 export default async (req, res) => {
-    let _ = await (await fetch('https://api.github.com/users/umutxyp/repos', {
+    let _ = await (await fetch('https://api.github.com/users/helixoo/repos', {
         headers: {
             Authorization: 'token '+key1+key2
         }
