a1454f487d347db77bb25d2c64ac330dc3e925e6
Helixoo <104375707+Helixoo@users.noreply.github.com>
2022-07-29 17:39:49 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -37,7 +37,7 @@ function MyApp({ Component, pageProps }) {
     });
   return (<>
     <Head>
-        <title>Helixo</title>
+        <title>Helixo's Portfolio </title>
         <link rel="shortcut icon" href="https://media.discordapp.net/attachments/977522571945848912/1002577802106781716/86980d74a8314c6aa832808268419ed2.png" type="image/x-icon" ></link>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet" />
@@ -56,7 +56,7 @@ function MyApp({ Component, pageProps }) {
         <div style={{ zIndex: 99999 }} className="fixed bg-black/75 w-full h-screen flex justify-center items-center pointer-events-none">
             <div className="flex items-center gap-x-6 animate-pulse">
                 <div className="text-center">
-                    <p className="text-6xl mb-5 font-semibold">Helixo Portfolio</p>
+                    <p className="text-6xl mb-5 font-semibold">HLX</p>
                     <p className="uppercase text-xl font-semibold text-white"><i className="fal fa-spinner-third fa-spin" /></p>
                 </div>
             </div>
